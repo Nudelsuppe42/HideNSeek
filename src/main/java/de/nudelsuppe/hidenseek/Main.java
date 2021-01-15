0d7c6b33c5abd68268a58db555ceb35ab6fec7a9
Nudelsuppe_42_ <jufo.theo@gmail.com>
2021-01-15 20:08:09 +0100

    Hug

--- src/main/java/de/nudelsuppe/hidenseek/Main.java
+++ src/main/java/de/nudelsuppe/hidenseek/Main.java
@@ -5,6 +5,7 @@ import de.nudelsuppe.hidenseek.listeners.Death_Listener;
 import de.nudelsuppe.hidenseek.listeners.Join_Leafe_Listener;
 import de.nudelsuppe.hidenseek.utils.Config;
 import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -25,6 +26,11 @@ public final class Main extends JavaPlugin {
         //Config.loadConfig();
         //Vars.configFile = new File(getDataFolder(), "config.yml");
         Vars.world = Bukkit.getWorld(Vars.worldName);
+        Vars.world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+        Vars.world.setGameRule(GameRule.DO_MOB_SPAWNING, false);
+        Vars.world.setGameRule(GameRule.DO_WEATHER_CYCLE, false);
+        Vars.world.setGameRule(GameRule.DROWNING_DAMAGE, false);
+        Vars.world.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, true);
         Bukkit.getServer().getPluginManager().registerEvents(new Death_Listener(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new Join_Leafe_Listener(), this);
         instance.getCommand("hindeandseek").setExecutor(new Main_Command());
--- src/main/java/de/nudelsuppe/hidenseek/listeners/Damage_Listener.java
+++ src/main/java/de/nudelsuppe/hidenseek/listeners/Damage_Listener.java
@@ -1,11 +1,21 @@
 package de.nudelsuppe.hidenseek.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class Damage_Listener implements Listener {
 
     @EventHandler
-    
+    public static void onDamage(EntityDamageEvent event) {
+        if(event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            if(event.getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK && event.getCause() != EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK && event.getCause() != EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) {
+                event.setCancelled(true);
+            }
+
+        }
+    }
 
 }
